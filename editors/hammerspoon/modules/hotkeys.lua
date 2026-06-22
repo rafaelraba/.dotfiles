ca80cd8896eab87a6c636a2a68a1a2e55c5af7ef
Rafael Raba <rafaelrb40@gmail.com>
2026-06-22 13:49:40 -0500

    fix(wm): simplify window management to aerospace only

--- editors/hammerspoon/modules/hotkeys.lua
+++ editors/hammerspoon/modules/hotkeys.lua
@@ -1,14 +1,1 @@
-local constants = require("modules.constants")
-local resize = require("modules.resize")
-
-local hyper = constants.hyper
-
-hs.hotkey.bind(hyper, "=", function()
-  resize.resizeFocusedWindow(constants.resizeStep)
-end)
-
-hs.hotkey.bind(hyper, "-", function()
-  resize.resizeFocusedWindow(-constants.resizeStep)
-end)
-
 return {}
